7038757091b1db1b0e7273ce4e974d3d672b7e99
Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2021-04-10 14:39:27 +0200

    fix some minor psalm findings

--- metadata.php
+++ metadata.php
@@ -27,7 +27,6 @@
 
 use OxidEsales\Eshop\Core\InputValidator;
 use OxidEsales\Eshop\Core\ViewConfig;
-use OxidProfessionalServices\PasswordPolicy\Controller\Admin\OxpsPasswordPolicyAdmin;
 use OxidProfessionalServices\PasswordPolicy\Core\PasswordPolicyConfig;
 use OxidProfessionalServices\PasswordPolicy\Core\PasswordPolicyValidator;
 use OxidProfessionalServices\PasswordPolicy\Core\PasswordPolicyViewConfig;
@@ -58,7 +57,6 @@ $aModule = [
         ],
     'controllers' => [],
     'templates' => [
-        'admin_oxpspasswordpolicy.tpl' => 'oxps/passwordpolicy/views/admin/admin_oxpspasswordpolicy.tpl',
     ],
     'blocks' => [
         [
