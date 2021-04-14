541156661fa9fa86af112368302201cd7bdd98a9
moritz_demmer <moritz.demmer@oxid-esales.com>
2021-04-14 13:36:12 +0200

    added password check

---
+++ src/Api/PasswordCheck.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OxidProfessionalServices\PasswordPolicy\Api;
+use Enzoic\Enzoic;
+use Enzoic\PasswordType;
+use OxidProfessionalServices\PasswordPolicy\Core\PasswordPolicyConfig;
+
+class PasswordCheck
+{
+
+
+    public function isPasswordKnown(string $password): bool
+    {
+        $config = new PasswordPolicyConfig();
+        $apiCon = new Enzoic($config->getAPIKey(), $config->getSecretKey());
+
+        $result = $apiCon->checkPassword($password);
+        return $result !== null;
+    }
+
+}
