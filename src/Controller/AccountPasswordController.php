58fff266a618ae972bdb608a194ee7479770fe77
moritz_demmer <moritz.demmer@oxid-esales.com>
2021-04-16 15:11:43 +0200

    cleaned up the code

--- src/Controller/AccountPasswordController.php
+++ src/Controller/AccountPasswordController.php
@@ -2,6 +2,8 @@
 
 namespace OxidProfessionalServices\PasswordPolicy\Controller;
 
+use OxidEsales\Eshop\Core\InputValidator;
+
 class AccountPasswordController extends AccountPasswordController_parent
 {
     /**
@@ -24,38 +26,26 @@ class AccountPasswordController extends AccountPasswordController_parent
         $sNewPass = \OxidEsales\Eshop\Core\Registry::getConfig()->getRequestParameter('password_new', true);
         $sConfPass = \OxidEsales\Eshop\Core\Registry::getConfig()->getRequestParameter('password_new_confirm', true);
 
-        /** @var \OxidEsales\Eshop\Core\InputValidator $oInputValidator */
-        $oInputValidator = \OxidEsales\Eshop\Core\Registry::getInputValidator();
-        if (($oExcp = $oInputValidator->checkPassword($oUser, $sNewPass, $sConfPass, true))) {
-            switch ($oExcp->getMessage()) {
-                case \OxidEsales\Eshop\Core\Registry::getLang()->translateString('ERROR_MESSAGE_INPUT_EMPTYPASS'):
-                case \OxidEsales\Eshop\Core\Registry::getLang()->translateString('ERROR_MESSAGE_PASSWORD_TOO_SHORT'):
-                    return \OxidEsales\Eshop\Core\Registry::getUtilsView()->addErrorToDisplay(
-                        'ERROR_MESSAGE_PASSWORD_TOO_SHORT',
-                        false,
-                        true
-                    );
-                case \OxidEsales\Eshop\Core\Registry::getLang()->translateString('OXPS_PASSWORDPOLICY_PASSWORDSTRENGTH_ERROR_PASSWORD_KNOWN'):
-                    return \OxidEsales\Eshop\Core\Registry::getUtilsView()->addErrorToDisplay(
-                        'OXPS_PASSWORDPOLICY_PASSWORDSTRENGTH_ERROR_PASSWORD_KNOWN',
-                        false,
-                        true
-                    );
-                default:
-                    return \OxidEsales\Eshop\Core\Registry::getUtilsView()->addErrorToDisplay(
-                        'ERROR_MESSAGE_PASSWORD_DO_NOT_MATCH',
-                        false,
-                        true
-                    );
-            }
-        }
-
         if (!$sOldPass || !$oUser->isSamePassword($sOldPass)) {
             /** @var \OxidEsales\Eshop\Core\UtilsView $oUtilsView */
             $oUtilsView = \OxidEsales\Eshop\Core\Registry::getUtilsView();
 
             return $oUtilsView->addErrorToDisplay('ERROR_MESSAGE_CURRENT_PASSWORD_INVALID', false, true);
         }
+
+        /** @var \OxidEsales\Eshop\Core\InputValidator $oInputValidator */
+        $oInputValidator = \OxidEsales\Eshop\Core\Registry::getInputValidator();
+
+        if (($oExcp = $oInputValidator->checkPassword($oUser, $sNewPass, $sConfPass, true))) {
+            $tmpInputValidator = oxNew(InputValidator::class);
+            \OxidEsales\Eshop\Core\Registry::set(InputValidator::class, $tmpInputValidator);
+                    return \OxidEsales\Eshop\Core\Registry::getUtilsView()->addErrorToDisplay(
+                        $oExcp,
+                        false,
+                        true
+                    );
+        }
+
 
         // testing passed - changing password
         $oUser->setPassword($sNewPass);
