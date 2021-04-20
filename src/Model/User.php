33e6fd05b948fc3085b402a578e425f89f51258d
moritz_demmer <moritz.demmer@oxid-esales.com>
2021-04-20 17:34:23 +0200

    fixed bug that password is checked for an unauthorized user

--- src/Model/User.php
+++ src/Model/User.php
@@ -4,6 +4,7 @@ namespace OxidProfessionalServices\PasswordPolicy\Model;
 
 use OxidEsales\Eshop\Application\Controller\ForgotPasswordController;
 use OxidEsales\Eshop\Core\Exception\UserException;
+use OxidEsales\Eshop\Core\InputValidator;
 use OxidProfessionalServices\PasswordPolicy\Core\PasswordPolicyValidator;
 
 class User extends User_parent
@@ -16,8 +17,10 @@ class User extends User_parent
      */
     public function onLogin($userName, $password)
     {
-        $passValidator = new PasswordPolicyValidator();
-        if ($err = $passValidator->validatePassword($password)) {
+        /** @var PasswordPolicyValidator $passValidator */
+        $passValidator = oxNew(InputValidator::class);
+
+        if ($this->isLoaded() && $err = $passValidator->validatePassword($userName, $password)) {
             $forgotPass = new ForgotPasswordController();
             $forgotPass->forgotPassword();
             throw oxNew(UserException::class, $err->getMessage());
