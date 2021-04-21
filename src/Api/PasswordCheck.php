ef9f747ef85c5ff70d58ce00baafbbd1fae0c143
moritz_demmer <moritz.demmer@oxid-esales.com>
2021-04-21 12:05:38 +0200

    removed settings check and added it in the DataBreach Validator Class

--- src/Api/PasswordCheck.php
+++ src/Api/PasswordCheck.php
@@ -13,10 +13,6 @@ class PasswordCheck
     public function isPasswordKnown(string $password): bool
     {
         $config = new PasswordPolicyConfig();
-        if(!$config->getAPINeeded())
-        {
-            return false;
-        }
         $apiCon = new Enzoic($config->getAPIKey(), $config->getSecretKey());
         $result = $apiCon->checkPassword($password);
         return $result !== null;
