fab3a50a4c8ada8ac9f4029852554ebcead0091b
moritz_demmer <moritz.demmer@oxid-esales.com>
2021-04-21 17:08:14 +0200

    added credentials api

--- src/Api/PasswordCheck.php
+++ src/Api/PasswordCheck.php
@@ -3,19 +3,48 @@
 declare(strict_types=1);
 
 namespace OxidProfessionalServices\PasswordPolicy\Api;
+
 use Enzoic\Enzoic;
 use OxidProfessionalServices\PasswordPolicy\Core\PasswordPolicyConfig;
 
+/**
+ * Class PasswordCheck
+ * @package OxidProfessionalServices\PasswordPolicy\Api
+ */
 class PasswordCheck
 {
 
+    private $config;
+    private $apiCon;
 
+    /**
+     * PasswordCheck constructor.
+     */
+    public function __construct()
+    {
+        $this->config = new PasswordPolicyConfig();
+        $this->apiCon = new Enzoic($this->config->getAPIKey(), $this->config->getSecretKey());
+    }
+
+    /**
+     * @param string $password
+     * @return bool
+     */
     public function isPasswordKnown(string $password): bool
     {
-        $config = new PasswordPolicyConfig();
-        $apiCon = new Enzoic($config->getAPIKey(), $config->getSecretKey());
-        $result = $apiCon->checkPassword($password);
+        $result = $this->apiCon->checkPassword($password);
         return $result !== null;
+    }
+
+    /**
+     * @param string $username
+     * @param string $password
+     * @return bool
+     */
+    public function isCredentialsKnown(string $username, string $password): bool
+    {
+        $result = $this->apiCon->checkCredentials($username, $password);
+        return $result;
     }
 
 }
