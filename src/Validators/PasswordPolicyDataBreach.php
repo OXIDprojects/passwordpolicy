97ec7719f66b84ea0376fc4349ecd63a18ae404f
moritz <moritz.demmer@oxid-esales.com>
2021-05-05 16:16:54 +0200

    now logs when enzoic throws an error (not visible for user)

--- src/Validators/PasswordPolicyDataBreach.php
+++ src/Validators/PasswordPolicyDataBreach.php
@@ -5,23 +5,32 @@ namespace OxidProfessionalServices\PasswordPolicy\Validators;
 
 use OxidProfessionalServices\PasswordPolicy\Api\PasswordCheck;
 use OxidProfessionalServices\PasswordPolicy\Core\PasswordPolicyConfig;
+use Psr\Log\LoggerInterface;
 
 class PasswordPolicyDataBreach implements PasswordPolicyValidationInterface
 {
 
     private PasswordPolicyConfig $config;
     private PasswordCheck $passwordCheck;
+    private LoggerInterface $logger;
 
-    public function __construct(PasswordPolicyConfig $config, PasswordCheck $passwordCheck)
+    public function __construct(PasswordPolicyConfig $config, PasswordCheck $passwordCheck, LoggerInterface $logger)
     {
+        $this->logger = $logger;
         $this->config = $config;
         $this->passwordCheck = $passwordCheck;
     }
 
     public function validate(string $sUsername, string $sPassword)
     {
-        if ($this->config->getAPINeeded() && ($this->passwordCheck->isPasswordKnown($sPassword) || $this->passwordCheck->isCredentialsKnown($sUsername, $sPassword))) {
-            return 'OXPS_PASSWORDPOLICY_PASSWORDSTRENGTH_ERROR_PASSWORD_KNOWN';
+        try {
+            if ($this->config->getAPINeeded() && $this->passwordCheck->isPasswordKnown($sUsername, $sPassword)) {
+                return 'OXPS_PASSWORDPOLICY_PASSWORDSTRENGTH_ERROR_PASSWORD_KNOWN';
+            }
+        } catch (\RuntimeException $exception) {
+            $errorClass = get_class($exception);
+            $code = $exception->getCode();
+            $this->logger->warning("Enzoic API Error: $errorClass Code: $code");
         }
         return true;
     }
