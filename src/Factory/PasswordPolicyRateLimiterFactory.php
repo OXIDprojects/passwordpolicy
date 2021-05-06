7dd97f4ffcb76f8c668a51b4b57c963a2ff34c6f
moritz <moritz.demmer@oxid-esales.com>
2021-05-06 09:44:35 +0200

    now gets the selected ratelimiter from container

--- src/Factory/PasswordPolicyRateLimiterFactory.php
+++ src/Factory/PasswordPolicyRateLimiterFactory.php
@@ -2,29 +2,29 @@
 
 namespace OxidProfessionalServices\PasswordPolicy\Factory;
 
+use OxidEsales\EshopCommunity\Internal\Container\ContainerFactory;
 use OxidProfessionalServices\PasswordPolicy\Exception\LimiterNotFound;
-use OxidProfessionalServices\PasswordPolicy\Factory\RateLimiter\PasswordPolicyMemcached;
-use OxidProfessionalServices\PasswordPolicy\Factory\RateLimiter\PasswordPolicyAPCu;
 use OxidProfessionalServices\PasswordPolicy\Factory\RateLimiter\PasswordPolicyRateLimiterInterface;
+use Psr\Container\NotFoundExceptionInterface;
 
 class PasswordPolicyRateLimiterFactory
 {
-    private array $versionMap = [
-        "Memcached" => PasswordPolicyMemcached::class,
-        "APCu" => PasswordPolicyAPCu::class
-    ];
-
     /**
-     * @param string $version
+     * @param string $limiter
      * @return PasswordPolicyRateLimiterInterface
      * @throws LimiterNotFound
      */
-    public function getRateLimiter(string $version): PasswordPolicyRateLimiterInterface
+    public function getRateLimiter(string $limiter): PasswordPolicyRateLimiterInterface
     {
-        if (!isset($this->versionMap[$version])) {
+        $class = 'OxidProfessionalServices\PasswordPolicy\Factory\RateLimiter\\PasswordPolicy' . $limiter;
+        $container = ContainerFactory::getInstance()->getContainer();
+        try {
+            $driver = $container->get($class);
+        }
+        catch (NotFoundExceptionInterface $ex)
+        {
             throw new LimiterNotFound();
         }
-        $driver = new $this->versionMap[$version]();
         return $driver;
     }
 }
