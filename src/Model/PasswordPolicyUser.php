7072b50ba36adeddf97748a71a78cd4fd88626a6
moritz <moritz.demmer@oxid-esales.com>
2021-05-07 21:47:04 +0200

    temporary solution for admin users added

--- src/Model/PasswordPolicyUser.php
+++ src/Model/PasswordPolicyUser.php
@@ -3,6 +3,7 @@
 namespace OxidProfessionalServices\PasswordPolicy\Model;
 
 use OxidEsales\Eshop\Application\Controller\ForgotPasswordController;
+use OxidEsales\Eshop\Core\Exception\ConnectionException;
 use OxidEsales\Eshop\Core\Exception\UserException;
 use OxidEsales\Eshop\Core\InputValidator;
 use OxidEsales\Eshop\Core\Registry;
@@ -26,10 +27,14 @@ class PasswordPolicyUser extends PasswordPolicyUser_parent
     {
         /** @var PasswordPolicyValidator $passValidator */
         $passValidator = oxNew(InputValidator::class);
-        if (!isAdmin() && $this->isLoaded() && $err = $passValidator->validatePassword($userName, $password)) {
+        if ($this->isLoaded() && $err = $passValidator->validatePassword($userName, $password)) {
             $forgotPass = new ForgotPasswordController();
             $forgotPass->forgotPassword();
             $errorMessage = $err->getMessage() . '&nbsp' . Registry::getLang()->translateString('REQUEST_PASSWORD_AFTERCLICK');
+            if(isAdmin())
+            {
+                throw oxNew(ConnectionException::class, $errorMessage);
+            }
             throw oxNew(UserException::class, $errorMessage);
         }
         parent::onLogin($userName, $password);
@@ -51,8 +56,13 @@ class PasswordPolicyUser extends PasswordPolicyUser_parent
             $rateLimiter = (new PasswordPolicyRateLimiterFactory())->getRateLimiter($driverName)->getLimiter();
 
             try {
-                $rateLimiter->limit($userName, Rate::perMinute($config->getRateLimit()));
+                $rateLimiter->limit($userName, Rate::perDay($config->getRateLimit()));
             } catch (LimitExceeded $exception) {
+                if(isAdmin())
+                {
+                    throw oxNew(ConnectionException::class, 'OXPS_PASSWORDPOLICY_RATELIMIT_EXCEEDED');
+
+                }
                 throw oxNew(UserException::class, 'OXPS_PASSWORDPOLICY_RATELIMIT_EXCEEDED');
             }
         }
