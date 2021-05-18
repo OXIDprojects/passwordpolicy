b54fdf0bcf393be44e88e816c76bd0feb31ac976
moritz <moritz.demmer@oxid-esales.com>
2021-05-18 19:02:30 +0200

    cleaned up login

--- src/Controller/PasswordPolicyTwoFactorLogin.php
+++ src/Controller/PasswordPolicyTwoFactorLogin.php
@@ -21,7 +21,7 @@ class PasswordPolicyTwoFactorLogin extends FrontendController
     public function finalizeLogin()
     {
         $otp = (new Request())->getRequestEscapedParameter('otp');
-        $usr = Registry::getSession()->getVariable('usr2');
+        $usr = Registry::getSession()->getVariable('tmpusr');
         $user = oxNew(User::class);
         $user->load($usr);
         $secret = $user->oxuser__oxtotpsecret->value;
@@ -29,7 +29,16 @@ class PasswordPolicyTwoFactorLogin extends FrontendController
         $checkOTP = $TOTP->checkOTP($secret, $otp);
         if($checkOTP)
         {
+            Registry::getSession()->deleteVariable('tmpusr');
             Registry::getSession()->setVariable('usr', $usr);
         }
+    }
+
+    public function getBreadCrumb()
+    {
+        $aPath['title'] = Registry::getLang()->translateString('TWOFACTORAUTHLOGIN');
+        $aPath['link'] = $this->getLink();
+        $aPaths[] = $aPath;
+        return $aPaths;
     }
 }
