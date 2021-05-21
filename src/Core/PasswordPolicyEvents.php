61842bb002b2d9ce7167fe2899db4a7401c8c81a
moritz <moritz.demmer@oxid-esales.com>
2021-05-21 17:17:51 +0200

    made sure that views will be regenerated after views update

--- src/Core/PasswordPolicyEvents.php
+++ src/Core/PasswordPolicyEvents.php
@@ -4,6 +4,8 @@
 namespace OxidProfessionalServices\PasswordPolicy\Core;
 
 
+use OxidEsales\Eshop\Core\DbMetaDataHandler;
+
 class PasswordPolicyEvents
 {
     public static function onActivate()
@@ -11,9 +13,19 @@ class PasswordPolicyEvents
         $query = "ALTER TABLE oxuser ADD OXTOTPSECRET varchar(255) NOT NULL;";
         try {
             \OxidEsales\Eshop\Core\DatabaseProvider::getDb()->execute($query);
+            self::regenerateViews();
         }catch (\Exception $exception)
         {
         }
 
+    }
+
+    /**
+     * Regenerate views for changed tables
+     */
+    protected static function regenerateViews()
+    {
+        $oDbMetaDataHandler = oxNew(DbMetaDataHandler::class );
+        $oDbMetaDataHandler->updateViews();
     }
 }
