cad84fc06754e46ee24036112c2f4d203219d4f9
moritz <moritz.demmer@oxid-esales.com>
2021-09-30 19:56:57 +0200

    cleaning tmp folder when activating the module

--- src/Core/PasswordPolicyEvents.php
+++ src/Core/PasswordPolicyEvents.php
@@ -20,25 +20,26 @@ class PasswordPolicyEvents
     public static function onActivate()
     {
         $user = oxNew(User::class);
-        if(!(in_array('oxpstotpsecret',$user->getFieldNames()) && in_array('oxpsbackupcode',$user->getFieldNames()))) {
+        if (!(in_array('oxpstotpsecret', $user->getFieldNames()) && in_array('oxpsbackupcode', $user->getFieldNames()))) {
             $query = "ALTER TABLE oxuser ADD OXPSTOTPSECRET varchar(255) NOT NULL,
                                      ADD OXPSBACKUPCODE varchar(255) NOT NULL;";
             try {
                 DatabaseProvider::getDb()->execute($query);
                 self::regenerateViews();
             } catch (\Exception $exception) {
-                throw new UserException("Ein Fehler ist bei der Erzeugung der neuen Datenbankspalten aufgetreten: \n" . $exception);
+                throw oxNew(UserException::class, "Ein Fehler ist bei der Erzeugung der neuen Datenbankspalten aufgetreten: \n" . $exception);
             }
+            self::clearTmp();
         }
-            $viewConf = oxNew(ViewConfig::class);
-            $modulePath = $viewConf->getModulePath('oxpspasswordpolicy');
-            $filePath = $modulePath . 'twofactor.config.inc.php';
-            if(!file_exists($filePath))
-            {
-                $key = self::generateKey();
-                file_put_contents($filePath, '<?php 
+
+        $viewConf = oxNew(ViewConfig::class);
+        $modulePath = $viewConf->getModulePath('oxpspasswordpolicy');
+        $filePath = $modulePath . 'twofactor.config.inc.php';
+        if (!file_exists($filePath)) {
+            $key = self::generateKey();
+            file_put_contents($filePath, '<?php 
 $this->key=' . "\"$key\";");
-            }
+        }
     }
 
 
@@ -48,9 +49,58 @@ $this->key=' . "\"$key\";");
         $hashedKey = base64_encode($key);
         return $hashedKey;
     }
+
     protected static function regenerateViews()
     {
-        $oDbMetaDataHandler = oxNew(DbMetaDataHandler::class );
+        $oDbMetaDataHandler = oxNew(DbMetaDataHandler::class);
         $oDbMetaDataHandler->updateViews();
+    }
+
+    public static function clearTmp($clearFolderPath = '')
+    {
+        $folderPath = self::_getFolderToClear($clearFolderPath);
+        $directoryHandler = opendir($folderPath);
+
+        if (!empty($directoryHandler)) {
+            while (false !== ($fileName = readdir($directoryHandler))) {
+                $filePath = $folderPath . DIRECTORY_SEPARATOR . $fileName;
+                self::_clear($fileName, $filePath);
+            }
+
+            closedir($directoryHandler);
+        }
+
+        return true;
+    }
+
+    protected static function _getFolderToClear($clearFolderPath = '')
+    {
+        $templateFolderPath = (string)\OxidEsales\Eshop\Core\Registry::getConfig()->getConfigParam('sCompileDir');
+
+        if (!empty($clearFolderPath) and (strpos($clearFolderPath, $templateFolderPath) !== false)) {
+            $folderPath = $clearFolderPath;
+        } else {
+            $folderPath = $templateFolderPath;
+        }
+
+        return $folderPath;
+    }
+
+    /**
+     * Check if resource could be deleted, then delete it's a file or
+     * call recursive folder deletion if it's a directory.
+     *
+     * @param string $fileName
+     * @param string $filePath
+     */
+    protected static function _clear($fileName, $filePath)
+    {
+        if (!in_array($fileName, ['.', '..', '.gitkeep', '.htaccess'])) {
+            if (is_file($filePath)) {
+                @unlink($filePath);
+            } else {
+                self::clearTmp($filePath);
+            }
+        }
     }
 }
